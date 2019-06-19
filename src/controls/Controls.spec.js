ad686397fda760d1b375e566bfc1597d70425720
Thierry Joux <tjoux@hotmail.com>
2019-06-19 15:52:01 -0700

    Completed some testings on Dashboard and display

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,37 @@
 // Test away!
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Controls from './Controls';
+
+import {render, fireEvent} from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each'
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Controls />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+// describe('Open/Close Gate button', () => {
+//     it('toggles  between open Gate and Close Gate after clicking', () => {
+//         let {getByText} = render(<Controls />);
+
+//         const openCloseBtn = getByText(/Close Gate/i);
+//         fireEvent.click(openCloseBtn);
+//         getByText(/Open Gate/i);
+//     })
+// })
+
+describe('Lock/Unlock Gate button', () => {
+    it('toggles  between Lock Gate and Unlock Gate after clicking', () => {
+        let {getByText} = render(<Controls />);
+
+        const openCloseBtn = getByText(/Close Gate/i);
+        const lockUnlockGate = getByText(/Lock Gate/i);
+
+        fireEvent.click(openCloseBtn);
+        fireEvent.click(lockUnlockGate);
+
+        getByText(/Unlock Gate/i);
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,40 @@
-// Test away!
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Display from './Display';
+
+import {render} from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each'
+
+import "jest-dom/extend-expect"; 
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Display />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+
+describe('display text', () => {
+    it('Checks default display values: unlocked and open', () => {
+        const {getByText} = render(<Display />);
+
+        getByText(/Unlocked/i);
+        getByText(/Open/i)
+    })
+})
+
+// describe('checks default display values are green', () => {
+//     const {getByText} = render(<Display />);
+
+//     const openCloseDisplay = getByText(/Open/i);
+//     const lockUnlockDisplay = getByText(/Unlocked/i);
+
+//     expect(openCloseDisplay).toHaveStyle('background-color: limegreen;')
+
+// })
+
+
+
+
+
+
